9401de91e47c17b70476ef501f14b5b42a0b922c
PukPukov <kvasov.les@gmail.com>
2023-02-14 12:07:22 +0500

    1.3.4

--- src/main/java/ru/ancap/commons/resource/ResourceExtractor.java
+++ src/main/java/ru/ancap/commons/resource/ResourceExtractor.java
@@ -5,13 +5,5 @@ import java.io.InputStream;
 public interface ResourceExtractor<T> {
     
     T extract(InputStream stream);
-
-    /**
-     * Called every time when resource loaded. Should check for conflicts and resolve them if found,
-     * and return resource with resolved conflict. <br> <br>
-     *
-     * Not called, if definitely no conflict - if user didn't have this resource before and wasn't able to change it.
-     */
-    InputStream resolveConflict(InputStream software, InputStream user);
     
 }
