a77f9b6218218617bbe60d8421c13912d7d27903
ancap-kun <vovanchikputin@vk.com>
2023-05-09 12:14:30 +0500

    1.8.13

--- src/main/java/ru/ancap/commons/map/GuaranteedMap.java
+++ src/main/java/ru/ancap/commons/map/GuaranteedMap.java
@@ -4,35 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import lombok.experimental.Delegate;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
 @AllArgsConstructor
 @ToString @EqualsAndHashCode
-@Deprecated
 public class GuaranteedMap<K, V> implements Map<K, V> {
     
-    @Delegate(excludes = LombokGetExclude.class)
+    @Delegate
     private final Map<K, V> base;
-    private final Supplier<V> guarantor;
     
     public GuaranteedMap(Supplier<V> guarantor) {
-        this(new ConcurrentHashMap<>(), guarantor);
+        this(SafeMap.<K, V>builder()
+            .guaranteed(guarantor)
+            .build()
+        );
     }
-
-    @Override
-    public @NotNull V get(Object key) {
-        V value = this.base.get(key);
-        if (value == null) {
-            value = this.guarantor.get();
-            this.base.put((K) key, value);
-        }
-        return value;
-    }
-
-    private interface LombokGetExclude<T> { T get(Object key); }
     
 }
