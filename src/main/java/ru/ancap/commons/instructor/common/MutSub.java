c0c1a75a4f2653fb13eaae64edf051a981001011
PukPukov <vovanchikputin@vk.com>
2023-12-10 23:52:00 +0500

    Initial value to MutSub

--- src/main/java/ru/ancap/commons/instructor/common/MutSub.java
+++ src/main/java/ru/ancap/commons/instructor/common/MutSub.java
@@ -1,11 +1,14 @@
 package ru.ancap.commons.instructor.common;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import ru.ancap.commons.instructor.EventBus;
 import ru.ancap.commons.instructor.SimpleEventBus;
 
 /**
  * Subscribe to field mutation.
  */
+@NoArgsConstructor @AllArgsConstructor
 public class MutSub<T> {
 
     public final EventBus<T> onChange = new SimpleEventBus<>();
