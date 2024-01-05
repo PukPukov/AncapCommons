3067ec2e1e7976b4b1d6c4fb9c7c36f0a5a1cc15
PukPukov <kvasov.les@gmail.com>
2024-01-05 06:22:13 +0500

    fluent-style accessors in pair

--- src/main/java/ru/ancap/commons/Pair.java
+++ src/main/java/ru/ancap/commons/Pair.java
@@ -3,12 +3,17 @@ package ru.ancap.commons;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import lombok.experimental.Accessors;
 
 @Data
 @ToString @EqualsAndHashCode
+@Accessors(fluent = true)
 public class Pair<K, V> {
     
     private final K key;
     private final V value;
+
+    @Deprecated(forRemoval = false) public K getKey()   { return this.key(); }
+    @Deprecated(forRemoval = false) public V getValue() { return this.value(); }
     
 }
