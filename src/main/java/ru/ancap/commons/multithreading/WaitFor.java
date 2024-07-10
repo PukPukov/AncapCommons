fd536cea3154f4ba04ec7d93870fba7ad9f4d410
PukPukov <vovanchikputin@vk.com>
2024-07-10 21:37:58 +0500

    fix potential visibility/race condition issue

--- src/main/java/ru/ancap/commons/multithreading/WaitFor.java
+++ src/main/java/ru/ancap/commons/multithreading/WaitFor.java
@@ -4,28 +4,26 @@ import lombok.EqualsAndHashCode;
 import lombok.SneakyThrows;
 import lombok.ToString;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
 
-/**
- * This is not event bus! Should be used to make only one thread wait (amount of producer threads can be unlimited).
- */
 @ToString @EqualsAndHashCode
 public class WaitFor<T> {
     
-    private final BlockingQueue<T> objectStore = new ArrayBlockingQueue<>(1);
+    private final CountDownLatch latch = new CountDownLatch(1);
     
+    // no volatile because of memory effect of count down latch
     private T store;
     
     @SneakyThrows
     public T get() {
-        if (this.store == null) this.store = this.objectStore.take();
+        this.latch.await();
         return this.store;
     }
     
     @SneakyThrows
     public void put(T object) {
-        this.objectStore.offer(object);
+        this.store = object;
+        this.latch.countDown();
     }
     
 }
