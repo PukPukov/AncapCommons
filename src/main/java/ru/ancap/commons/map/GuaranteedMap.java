2b6e07801c277b8ea223c95fcd269521ade6ea63
PukPukov <vovanchikputin@vk.com>
2024-08-05 03:02:36 +0500

    make guaranteed map constructor private, making error raise earlier because usage like that is 100% bug because map in that case does not provide any guaranties, add ability to choose base map properly

--- src/main/java/ru/ancap/commons/map/GuaranteedMap.java
+++ src/main/java/ru/ancap/commons/map/GuaranteedMap.java
@@ -1,26 +1,32 @@
 package ru.ancap.commons.map;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.Delegate;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
-@AllArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @ToString @EqualsAndHashCode
 public class GuaranteedMap<K, V> implements Map<K, V> {
     
     @Delegate
     private final Map<K, V> base;
     
-    public GuaranteedMap(Supplier<V> guarantor) {
-        this(SafeMap.<K, V>builder()
+    public GuaranteedMap(Map<K, V> base, Supplier<V> guarantor) {
+        this(SafeMap.builder(base)
             .guaranteed(guarantor)
             .build()
         );
+    }
+    
+    public GuaranteedMap(Supplier<V> guarantor) {
+        this(new HashMap<>(), guarantor);
     }
     
     public @NotNull V getExplicitlyPlaced(Object key) {
