7a8c374b9b806e4c2fc3d0739c1989e93f3505c1
PukPukov <vovanchikputin@vk.com>
2024-08-07 07:35:48 +0500

    add caller information

--- src/main/java/ru/ancap/commons/debug/AncapDebug.java
+++ src/main/java/ru/ancap/commons/debug/AncapDebug.java
@@ -1,14 +1,17 @@
 package ru.ancap.commons.debug;
 
 import lombok.EqualsAndHashCode;
+import lombok.SneakyThrows;
 import lombok.ToString;
 import net.jcip.annotations.ThreadSafe;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
@@ -19,7 +22,7 @@ import java.util.stream.Collectors;
 @ToString @EqualsAndHashCode
 public class AncapDebug {
     
-    public static volatile Consumer<String> OUTPUT_CONSUMER = string -> System.out.println("DEBUG "+string);
+    public static volatile Consumer<String> OUTPUT_CONSUMER = System.out::println;
     
     public static <T> T debugThrough(@Nullable T main, Object... additional) {
         var objects = new ArrayList<>();
@@ -32,6 +35,12 @@ public class AncapDebug {
     
     public static void debug(@Nullable Object... objects) {
         StringBuilder debug = new StringBuilder();
+        String callerInformation = caller()
+            .map(StackTraceElement::toString)
+            .orElse("null");
+        
+        debug.append("=== DEBUG in ").append(callerInformation).append(" ===");
+        debug.append("\n");
         for (int i = 0; i < objects.length; i++) {
             Object object = objects[i];
             if (i != 0) debug.append(" ");
@@ -45,14 +54,14 @@ public class AncapDebug {
     public static void debugArray(@Nullable Object[] array) {
         AncapDebug.debug(new Object[]{array});
     }
-
+    
     @NotNull
     private static String stringValueOf(@Nullable Object object) {
         if (object == null)                    return "null";
         else if (!object.getClass().isArray()) return object.getClass().getName()+"{"+object+"}";
         else                                   return arrayObjectToString(object);
     }
-
+    
     private static String arrayObjectToString(@NotNull Object object) {
         if (object instanceof Object []) return object.getClass().getComponentType().getName()+"[]{"+ Arrays.stream(((Object[]) object))
             .map(AncapDebug::stringValueOf)
@@ -67,9 +76,16 @@ public class AncapDebug {
         else if (object instanceof double  []) return "double[]{"  +Arrays.toString((double  []) object)+"}";
         else throw new IllegalStateException();
     }
-
+    
     private static void soloDebug(String string) {
         AncapDebug.OUTPUT_CONSUMER.accept(string);
     }
-
+    
+    private static Optional<StackTraceElement> caller() {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        
+        if (stackTrace.length > 3) return Optional.of(stackTrace[3]);
+        else return Optional.empty();
+    }
+    
 }
