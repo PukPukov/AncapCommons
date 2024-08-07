1f74ba8c134947d5ff3d027d319aeb2bf2de5ce9
PukPukov <kvasov.les@gmail.com>
2024-08-07 10:19:55 +0500

    add spaces highlighting to array contents and enhance code aligning

--- src/main/java/ru/ancap/commons/debug/AncapDebug.java
+++ src/main/java/ru/ancap/commons/debug/AncapDebug.java
@@ -155,17 +155,17 @@ public class AncapDebug {
     }
     
     private static String arrayObjectToString(@NotNull Object object) {
-        if (object instanceof Object []) return simplifiedName(object.getClass().getComponentType())+"[]{"+ Arrays.stream(((Object[]) object))
+        if (object instanceof Object []) return simplifiedName(object.getClass().getComponentType())+"[]{   "+ Arrays.stream(((Object[]) object))
             .map(AncapDebug::stringValueOf)
-            .collect(Collectors.joining(", "))+"}";
-        else if (object instanceof boolean []) return "boolean[]{" + pwacs(ArrayUtils.toObject((boolean []) object))+"}";
-        else if (object instanceof byte    []) return "byte[]{"    + pwacs(ArrayUtils.toObject((byte    []) object))+"}";
-        else if (object instanceof short   []) return "short[]{"   + pwacs(ArrayUtils.toObject((short   []) object))+"}";
-        else if (object instanceof char    []) return "char[]{"    + pwacs(ArrayUtils.toObject((char    []) object))+"}";
-        else if (object instanceof int     []) return "int[]{"     + pwacs(ArrayUtils.toObject((int     []) object))+"}";
-        else if (object instanceof long    []) return "long[]{"    + pwacs(ArrayUtils.toObject((long    []) object))+"}";
-        else if (object instanceof float   []) return "float[]{"   + pwacs(ArrayUtils.toObject((float   []) object))+"}";
-        else if (object instanceof double  []) return "double[]{"  + pwacs(ArrayUtils.toObject((double  []) object))+"}";
+            .collect(Collectors.joining(", "))+"   }";
+        else if (object instanceof boolean[]) return "boolean[]{   "+pwacs(ArrayUtils.toObject( (boolean[]) object))+"   }";
+        else if (object instanceof   short[]) return   "short[]{   "+pwacs(ArrayUtils.toObject(   (short[]) object))+"   }";
+        else if (object instanceof    byte[]) return    "byte[]{   "+pwacs(ArrayUtils.toObject(    (byte[]) object))+"   }";
+        else if (object instanceof    char[]) return    "char[]{   "+pwacs(ArrayUtils.toObject(    (char[]) object))+"   }";
+        else if (object instanceof     int[]) return     "int[]{   "+pwacs(ArrayUtils.toObject(     (int[]) object))+"   }";
+        else if (object instanceof    long[]) return    "long[]{   "+pwacs(ArrayUtils.toObject(    (long[]) object))+"   }";
+        else if (object instanceof   float[]) return   "float[]{   "+pwacs(ArrayUtils.toObject(   (float[]) object))+"   }";
+        else if (object instanceof  double[]) return  "double[]{   "+pwacs(ArrayUtils.toObject(  (double[]) object))+"   }";
         else throw new IllegalStateException();
     }
     
